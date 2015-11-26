6bb56554ac571e7fe442a513fb11a429babf82f0
Pedro Costa <PCosta@I9.innovagency.com>
2015-11-26 16:07:14 +0000

    Initial

---
+++ image-share/image_share.js
@@ -0,0 +1,27 @@
+/// <reference path=".typescript/meteor.d.ts" />
+if (Meteor.isClient) {
+  var img_data = [{
+    img_src: "TheWitcher3Skyrim-600x250.jpg",
+    img_alt: "witcher atchim away"
+  },
+  {
+    img_src: "TheWitcher3Skyrim-600x250.jpg",
+    img_alt: "witcher axing away"
+  },
+  {
+    img_src: "TheWitcher3Skyrim-600x250.jpg",
+    img_alt: "witcher swording away"
+  }]
+  //the name given to the property must match the name on the each object in the template "i"
+  Template.images.helpers({i:img_data})
+  //Events for a given template
+  Template.images.events({
+    'click .js-image':function (events) {
+      $(event.target).css('width', "50px")
+    }
+  })
+}
+
+if (Meteor.isServer) {
+  
+}
