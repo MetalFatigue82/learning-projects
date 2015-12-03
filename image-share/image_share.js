55a2f1e6640cdc90a2d0b81d515091b3bd669ae8
Pedro Costa <pedrorcosta82@gmail.com>
2015-12-03 18:41:17 +0000

    Week 2 - Implement image adding with a Bootstrap Modal - FINAL

--- image-share/image_share.js
+++ image-share/image_share.js
@@ -4,7 +4,9 @@ Images = new Mongo.Collection("images")
 
 if (Meteor.isClient) {
   //the name given to the property must match the name on the each object in the template "i"
-  Template.images.helpers({ i: Images.find() });
+  Template.images.helpers({ i: 
+      Images.find({}, {sort: {createdOn: -1, rating: -1}}) 
+    });
   //Events for a given template
   Template.images.events({
     'click .js-image': function (event) {
@@ -20,6 +22,38 @@ if (Meteor.isClient) {
         console.log("deleted image: " + imageID);
         Images.remove({ "_id": imageID });
       });
+    },
+    'click .js-rate-image': function (event) {
+      var rating = $(event.currentTarget).data('userrating');
+      Images.update(this["data-id"], {
+        $set: {"rating": rating}
+      });
+      console.log('star clicked ' + rating);
+    },
+    'click .js-new-image': function (event) {
+      $('#images_add_form').modal('show');
     }
   });
+  Template.images_add_form.events({
+    'submit .js-save': function (event) {
+      var img_src, img_alt;
+      
+      img_src = event.target.img_src.value;
+      img_alt = event.target.img_alt.value;
+      Images.insert(
+					{
+						img_src: img_src,
+						img_alt: img_alt,
+            createdOn: new Date()
+					}
+					)
+      $('#images_add_form').modal('hide');
+      console.log("src: "+img_src + " alt: "+img_alt);
+      return false;
+    } 
+  });
+  
+  // Template.images_add_form.helpers({ i: 
+  //     Images.find({}, {sort: {rating: -1}}) 
+  //   });
 }
