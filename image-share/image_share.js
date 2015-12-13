7634d7921d8adc61f1f4ac18d17edd01aa2b571d
MetalFatigue <pedrorcosta82@gmail.com>
2015-12-13 14:05:19 +0000

    Week 3 - FINAL

--- image-share/image_share.js
+++ image-share/image_share.js
@@ -3,30 +3,74 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
+  Session.set("imageLimit", 8);
+  var lastScrollTop = 0;
+  var $window = $(window);
+  var $document = $(document);
+  $window.scroll(function () {
+    
+    //we are near the end of window
+    if ($window.scrollTop() + $window.height() > $document.height() - 100) {
+      //Where are we in the page
+      var scrollTop = $(this).scrollTop();
+      //test if we are going down
+      if (scrollTop > lastScrollTop) {
+        Session.set("imageLimit", Session.get("imageLimit") + 4);
+      }
+      lastScrollTop = scrollTop;
+    }
+  });
+
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
   });
+  function getUsername(user_id) {
+    var user = Meteor.users.findOne({ _id: user_id });
+    if (user) {
+      return user.username;
+    }
+    else {
+      return "anon";
+    }
+  }
   //the name given to the property must match the name on the each object in the template "i"
   Template.images.helpers({
-    i: Images.find({}, {
-      sort: {
-        createdOn: -1,
-        rating: -1
-      }
-    }),
-    getUsername: function(user_id) {
-      var user = Meteor.users.findOne({_id: user_id});
-      if (user) {
-        return user.username;
+    i: function () {
+      if (Session.get("userFilter")) {
+        return Images.find({ createdBy: Session.get("userFilter") }, {
+          sort: {
+            createdOn: -1,
+            rating: -1
+          },
+          limit: Session.get("imageLimit")
+        })
       }
       else {
-        return "anon";
+        return Images.find({}, {
+          sort: {
+            createdOn: -1,
+            rating: -1
+          },
+          limit: Session.get("imageLimit")
+        })
+      }
+    },
+    getUsername: getUsername,
+    getFilterUsername: function () {
+      return getUsername(Session.get("userFilter"));
+    },
+    filtering_images: function () {
+      if (Session.get("userFilter")) {
+        return true;
+      }
+      else {
+        return false;
       }
     }
   });
 
   Template.body.helpers({
-    username: function() {
+    username: function () {
       if (Meteor.user()) {
         return Meteor.user().username;
         // return Meteor.user().emails[0].address;
@@ -39,23 +83,23 @@ if (Meteor.isClient) {
 
   //Events for a given template
   Template.images.events({
-    'click .js-image': function(event) {
+    'click .js-image': function (event) {
       if ($(event.target).css('width') == "50px")
         $(event.target).css('width', "");
       else
         $(event.target).css('width', "50px");
 
     },
-    'click .js-del-image': function(event) {
+    'click .js-del-image': function (event) {
       var imageID = this._id;
-      $('#' + imageID).hide("slow", function() {
+      $('#' + imageID).hide("slow", function () {
         console.log("deleted image: " + imageID);
         Images.remove({
           "_id": imageID
         });
       });
     },
-    'click .js-rate-image': function(event) {
+    'click .js-rate-image': function (event) {
       var rating = $(event.currentTarget).data('userrating');
       Images.update(this["data-id"], {
         $set: {
@@ -64,12 +108,18 @@ if (Meteor.isClient) {
       });
       console.log('star clicked ' + rating);
     },
-    'click .js-new-image': function(event) {
+    'click .js-new-image': function (event) {
       $('#images_add_form').modal('show');
+    },
+    'click .js-set-image-filter': function (event) {
+      Session.set("userFilter", this.createdBy);
+    },
+    'click .js-unset-image-filter': function () {
+      Session.set("userFilter", undefined);
     }
   });
   Template.images_add_form.events({
-    'submit .js-save': function(event) {
+    'submit .js-save': function (event) {
       var img_src, img_alt;
 
       img_src = event.target.img_src.value;
@@ -87,8 +137,4 @@ if (Meteor.isClient) {
       return false;
     }
   });
-
-  // Template.images_add_form.helpers({ i: 
-  //     Images.find({}, {sort: {rating: -1}}) 
-  //   });
 }
