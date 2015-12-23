728913f69c801d29811256a6fc11dc33ffb56beb
Pedro Costa <pedrorcosta82@gmail.com>
2015-12-23 14:15:06 +0000

    Week 4 - Final

--- image-share/client/main.js
+++ image-share/client/main.js
@@ -1,3 +1,36 @@
+///Routing
+Router.configure({
+    layoutTemplate: "ApplicationLayout"
+});
+
+Router.route('/', function() {
+    this.render('welcome',{
+        to:"main"
+    });
+});
+
+Router.route('/images', function() {
+    this.render('navbar',{
+        to:"navbar"
+    });
+    this.render('images',{
+        to:"main"
+    });
+});
+
+Router.route('/image/:_id', function() {
+    this.render('navbar',{
+        to:"navbar"
+    });
+    this.render('image',{
+        to:"main",
+        data: function () {
+            return Images.findOne({_id:this.params._id})
+        }
+    });
+});
+
+//  Infinity scroll
 Session.set("imageLimit", 8);
 var lastScrollTop = 0;
 var $window = $(window);
@@ -16,6 +49,8 @@ $window.scroll(function() {
     }
 });
 
+
+//config
 Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
 });
