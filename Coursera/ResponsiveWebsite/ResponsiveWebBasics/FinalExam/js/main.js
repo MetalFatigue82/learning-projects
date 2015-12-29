f2269573b840c81a40dce45a9551a27a5f7dbec4
MetalFatigue82 <pedrorcosta82@gmail.com>
2015-12-29 03:13:02 +0000

    Responsive web basics - Final Exam - Final

--- Coursera/ResponsiveWebsite/ResponsiveWebBasics/FinalExam/js/main.js
+++ Coursera/ResponsiveWebsite/ResponsiveWebBasics/FinalExam/js/main.js
@@ -1,7 +1,31 @@
 var main = (function ($) {
     //private methods
+    
     function privateInit() {
-        
+        var $window = $(window);
+
+        $('section[data-type="background"]').each(function () {
+            // declare the variable to affect the defined data-type
+            var $scroll = $(this);
+
+            $(window).scroll(function () {
+                // HTML5 proves useful for helping with creating JS functions!
+                // also, negative value because we're scrolling upwards                             
+                var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+         
+                // background position
+                var coords = '50% ' + yPos + 'px';
+ 
+                // move the background
+                $scroll.css({ backgroundPosition: coords });
+            }); // end window scroll
+        });  // end section function
+            
+        var counter = 0;
+        $("section#home button").on("click button", function () {
+            counter++;
+            $("div.modal .modal-body p").html("I was shown " + counter + " times!");
+        });
     }
     
     //public methods
@@ -10,6 +34,6 @@ var main = (function ($) {
     };
 })(jQuery);
 
-$(function() {
+$(function () {
     main.init();
 });
