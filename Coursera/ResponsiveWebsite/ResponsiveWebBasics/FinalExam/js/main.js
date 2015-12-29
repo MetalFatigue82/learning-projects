551582489e47b75cd268c9c6207277c84e150502
MetalFatigue82 <pedrorcosta82@gmail.com>
2015-12-29 00:13:06 +0000

    Responsive web basics - Final Exam - Skeleton

---
+++ Coursera/ResponsiveWebsite/ResponsiveWebBasics/FinalExam/js/main.js
@@ -0,0 +1,15 @@
+var main = (function ($) {
+    //private methods
+    function privateInit() {
+        
+    }
+    
+    //public methods
+    return {
+        init: privateInit
+    };
+})(jQuery);
+
+$(function() {
+    main.init();
+});
